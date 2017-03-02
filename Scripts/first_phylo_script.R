b6452f03dca7ced9c6581f3d636215f8bd962d5c
dbuona <dbuonaiuto@g.harvard.edu>
2017-03-02 15:52:24 -0500

    Worked on additions to no avail

--- Scripts/first_phylo_script.R
+++ Scripts/first_phylo_script.R
@@ -34,33 +34,36 @@ namelist<-unique(anthy$name)
 to.prune<-which(!names.intree%in%namelist)
 pruned.by.anthy<-drop.tip(treee,to.prune)
 ##did it work? Let's check
+pruned.by.anthy
 plot(pruned.by.anthy)
 mytree.names<-pruned.by.anthy$tip.label
-### 108 of my species were in the tree. Which species didn't male it
-###Add in the remaining ones, need to figure out how to do this with out dropping banch lengths, perhaps with pex function congeneric.merge
+mytree.names<-pruned.by.anthy$tip.label
+setdiff(namelist,mytree.names) 
+### 108 of my species were in the tree. 
+#Add in the remaining ones, need to figure out how to do this with out dropping banch lengths, perhaps with pez function congeneric.merge
 #species<-c("Acer_barbatum","Acer_nigrum","Aesculus_octandra","Carya_aquatica","Carya_illinoiensis","Carya_laciniosa","Celtis_tenuifolia","Fraxinus_pensylvanica","Gymnocladus_dioicus","Malus_coronaria","Populus_heterophylla","Prunus_nigra","Quercus_prinoides","Quercus_coccinea","Quercus_ellipsoidalis","Quercus_douglasii","Quercus_muehlenbergii","Quercus_nuttallii","Quercus_phellos","Quercus_prinus","Salix_nigra","Sorbus_americana","Tilia_heterophylla","Ulmus_thomasii")
- #for(i in 1:length(species)) pruned.by.anthy<-add.species.to.genus(pruned.by.anthy,species[i],where="random")
- #plotTree(pruned.by.anthy,ftype="i")
- mytree.names<-pruned.by.anthy$tip.label
- setdiff(namelist,mytree.names) 
- ##forwhatever reason, 15 species could not be added (8 quercus, 1 ulmus, 1 tilia, sorbus, salix, populus and gymocladus)
+#  for(i in 1:length(species)) pruned.by.anthy<-add.species.to.genus(pruned.by.anthy,species[i],where="root")
+# plotTree(pruned.by.anthy,ftype="i")
+
+ ##this step is also dealing with removing or adding species not on zanne tree. deal with thisa gain in the future
  #for now, I will subset to exclude them
-anthy<-filter(anthy,name != "Gymnocladus_dioicus")
- anthy<-filter(anthy,name !="Populus_heterophylla")
- anthy<-filter(anthy,name !="Prunus_nigra") 
- anthy<-filter(anthy,name !="Quercus_prinoides")
- anthy<-filter(anthy,name !="Quercus_coccinea")
- anthy<-filter(anthy,name !="Quercus_ellipsoidalis" )
- anthy<-filter(anthy,name != "Quercus_douglasii")
- anthy<-filter(anthy,name != "Quercus_muehlenbergii")
- anthy<-filter(anthy,name != "Quercus_nuttallii")
- anthy<-filter(anthy,name !="Quercus_phellos")
- anthy<-filter(anthy,name !="Quercus_prinus")
- anthy<-filter(anthy,name != "Salix_nigra")
- anthy<-filter(anthy,name !="Sorbus_americana")
- anthy<-filter(anthy,name !="Tilia_heterophylla")
- anthy<-filter(anthy,name !="Ulmus_thomasii")
+#anthy<-filter(anthy,name != "Gymnocladus_dioicus")
+ #anthy<-filter(anthy,name !="Populus_heterophylla")
+ #anthy<-filter(anthy,name !="Prunus_nigra") 
+ #anthy<-filter(anthy,name !="Quercus_prinoides")
+ #anthy<-filter(anthy,name !="Quercus_coccinea")
+ #anthy<-filter(anthy,name !="Quercus_ellipsoidalis" )
+ #anthy<-filter(anthy,name != "Quercus_douglasii")
+ #anthy<-filter(anthy,name != "Quercus_muehlenbergii")
+ #anthy<-filter(anthy,name != "Quercus_nuttallii")
+ #anthy<-filter(anthy,name !="Quercus_phellos")
+ #anthy<-filter(anthy,name !="Quercus_prinus")
+ #anthy<-filter(anthy,name != "Salix_nigra")
+ #anthy<-filter(anthy,name !="Sorbus_americana")
+ #anthy<-filter(anthy,name !="Tilia_heterophylla")
+# anthy<-filter(anthy,name !="Ulmus_thomasii")
 ##species list
+ 
 namelist<-unique(anthy$name)
 setdiff(namelist,mytree.names) 
 ###okay, now there is no differentce between namelist and mytree.names
