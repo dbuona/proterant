0a8411f0ceffadd029c6faa0eda4f5eaaa50630c
dbuona <dbuonaiuto@g.harvard.edu>
2017-03-07 17:42:02 -0500

    filled in much data set

--- Scripts/hf_offset_calcs.R
+++ Scripts/hf_offset_calcs.R
@@ -16,12 +16,12 @@ hf<-read.csv("hf003-05-mean-ind.csv",header=TRUE)
 hf<-mutate(hf, buds_seq = bb.jd -fbb.jd)
 hf<-mutate(hf, functional_seq = l75.jd -fopn.jd)
 hf<-mutate(hf, inter_seq=bb.jd-fopn.jd)
-library(dplyr)
+
 by_species <- hf %>% group_by(species)
-by_species<-by_species %>% summarise(mean(functional_seq, na.rm = TRUE))
+by_species<-by_species %>% summarise(mean(functional_seq, na.rm = TRUE), sd(functional_seq, na.rm = TRUE))
 
 by_species2 <- hf %>% group_by(species)
-by_species2<-by_species2 %>% summarise(mean(inter_seq, na.rm = TRUE))
+by_species2<-by_species2 %>% summarise(mean(inter_seq, na.rm = TRUE),sd(inter_seq, na.rm = TRUE))
 
 by_species3 <- hf %>% group_by(species)
-by_species3<-by_species3 %>% summarise(mean(buds_seq, na.rm = TRUE))
+by_species3<-by_species3 %>% summarise(mean(buds_seq, na.rm = TRUE),sd(buds_seq, na.rm = TRUE))
