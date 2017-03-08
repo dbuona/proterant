def53d01e9b153d0aef9bcf7961e80a7bf6b3007
dbuona <dbuonaiuto@g.harvard.edu>
2017-03-08 16:56:59 -0500

    started ggd work on harvard forest

---
+++ Scripts/gdd_calcs.R
@@ -0,0 +1,91 @@
+## 8 March 2017
+# GDD Script and Budburst for HF
+# Aim: To find the average accumulated growing degree days for Harvard Forest till budburst
+# using John O'Keefe's data
+
+# Clear workspace
+rm(list=ls()) # remove everything currently held in the R memory
+options(stringsAsFactors=FALSE)
+graphics.off()
+
+# Load libraries
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(lubridate)
+
+# Set Working Directory
+setwd("~/Documents/git/proterant")
+hf<- read.csv("data/WeatherData.csv", header=TRUE)
+d<-read.csv("data/hf003-06-mean-spp.csv",header=TRUE)
+
+# Harvard Forest
+# To double check my script is accurate
+hf<- filter(hf, Site == "hf")
+hf$gdd <- hf$AirT - 5
+hf$gdd <-ifelse(hf$gdd>0, hf$gdd, 0)
+hf$count <- ave(
+  hf$gdd, hf$Year, 
+  FUN=function(x) cumsum(c(0, head(x, -1)))
+)
+
+# Clean up dataframes and join
+hf<-rename(hf, year=Year)
+d<-d%>%
+  dplyr::select(year,species, bb.jd,l75.jd,fbb.jd,fopn.jd)
+d<- as.data.frame(rapply(object = d, f = round, classes = "numeric", how = "replace", digits = 0)) 
+df<-left_join(hf, d)
+###species with enough values
+df<-filter(df, species %in% c( "ACPE","ACRU", "ACSA","BEAL","FRAM","QURU"))
+###budburst
+df2<-df%>%
+  dplyr::select(year,species, JD, bb.jd,l75.jd,fbb.jd,fopn.jd, count) %>%
+  filter(year>=1990)
+df2$day<- ifelse(df2$JD==df2$bb.jd,df2$JD,NA)
+df2<-na.omit(df2)
+###l75
+df3<-df%>%
+  dplyr::select(year,species, JD, bb.jd,l75.jd,fbb.jd,fopn.jd, count) %>%
+  filter(year>=1990)
+df3$day<- ifelse(df3$JD==df3$l75.jd,df3$JD,NA)
+df3<-na.omit(df3)
+###nowfor fbb.jd
+df4<-df%>%
+  dplyr::select(year,species, JD, bb.jd,l75.jd,fbb.jd,fopn.jd, count) %>%
+  filter(year>=1990)
+df4$day<- ifelse(df4$JD==df4$fbb.jd,df4$JD,NA)
+df4<-na.omit(df4)
+##flowers open
+df5<-df%>%
+  dplyr::select(year,species, JD, bb.jd,l75.jd,fbb.jd,fopn.jd, count) %>%
+  filter(year>=1990)
+df5$day<- ifelse(df5$JD==df5$fopn.jd,df5$JD,NA)
+df5<-na.omit(df5)
+
+###what's this:
+df2<-group_by(df2,species)
+#moved this up becasue these were the only species with enough data coverage
+#df<-filter(df, species %in% c( "ACPE","ACRU", "ACSA","BEAL","FAGR","FRAM","QURU"))
+#bb
+q<-ggplot(df2, aes(x=year, y=bb.jd)) + geom_point()
+q+facet_wrap(~species)
+#l75
+q<-ggplot(df3, aes(x=year, y=l75.jd)) + geom_point( col="green")
+q+facet_wrap(~species)
+#fbb
+q<-ggplot(df4, aes(x=year, y=fbb.jd)) + geom_point(col="pink")
+q+facet_wrap(~species)
+#fopn
+q<-ggplot(df5, aes(x=year, y=fopn.jd)) + geom_point(col="red")
+q+facet_wrap(~species)
+
+###is count in each year significantly different? mean and sd 
+bb<-df2 %>% group_by(species) %>% summarise(mean(count), sd(count))
+l75<-df3 %>% group_by(species) %>% summarise(mean(count), sd(count))
+fbb<-df4 %>% group_by(species) %>% summarise(mean(count), sd(count))
+fopn<-df5 %>% group_by(species) %>% summarise(mean(count), sd(count))
+bb
+l75
+fbb
+fopn
+
