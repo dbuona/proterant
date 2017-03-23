a6b1d76d5999bb11816c26eaff8348eb9e4f962a
dbuona <dbuonaiuto@g.harvard.edu>
2017-03-23 08:24:56 -0400

    Working on phyla

--- Scripts/first_phylo_script.R
+++ Scripts/first_phylo_script.R
@@ -1,4 +1,5 @@
 ##DAn B start phylogenies on 1/19/2017
+#adapted for michigan trees data only on 3/12
 rm(list=ls()) # remove everything currently held in the R memory
 options(stringsAsFactors=FALSE)
 graphics.off()
@@ -24,8 +25,8 @@ treee<-read.tree("Vascular_Plants_rooted.dated.tre")
 names.intree<-treee$tip.label
 
 ### read in data, format it like Zanne
-anthy<-read.csv("proterant_ds.csv", header = TRUE)
-anthy$name<-paste(anthy$genus,anthy$species,sep="_")
+anthy<-read.csv("michigantrees_sequence.csv", header = TRUE)
+anthy$name<-paste(anthy$Genus,anthy$Species,sep="_")
 
 ###myspecies
 namelist<-unique(anthy$name)
@@ -37,9 +38,10 @@ pruned.by.anthy<-drop.tip(treee,to.prune)
 pruned.by.anthy
 plot(pruned.by.anthy)
 mytree.names<-pruned.by.anthy$tip.label
-mytree.names<-pruned.by.anthy$tip.label
+
 setdiff(namelist,mytree.names) 
-### 108 of my species were in the tree. 
+intersect(namelist,mytree.names)
+### 80 of my species were in the tree. 
 #Add in the remaining ones, need to figure out how to do this with out dropping banch lengths, perhaps with pez function congeneric.merge
 #species<-c("Acer_barbatum","Acer_nigrum","Aesculus_octandra","Carya_aquatica","Carya_illinoiensis","Carya_laciniosa","Celtis_tenuifolia","Fraxinus_pensylvanica","Gymnocladus_dioicus","Malus_coronaria","Populus_heterophylla","Prunus_nigra","Quercus_prinoides","Quercus_coccinea","Quercus_ellipsoidalis","Quercus_douglasii","Quercus_muehlenbergii","Quercus_nuttallii","Quercus_phellos","Quercus_prinus","Salix_nigra","Sorbus_americana","Tilia_heterophylla","Ulmus_thomasii")
 #  for(i in 1:length(species)) pruned.by.anthy<-add.species.to.genus(pruned.by.anthy,species[i],where="root")
@@ -73,26 +75,66 @@ namelist2<-final.df$name
 namelist2==mytree.names
 final.df$name== mytree.names
 ###matches!
+###make column for coded hysteranthy classes all 6
 final.df["pro"]<-NA
-
-  
-final.df$pro[final.df$Proteranthous == 1] <- 3
-final.df$pro[final.df$Proteranthous == 0] <- 2
-final.df$pro[final.df$Proteranthous== NA] <- 1
-  
-#which(anthy$name%in%pruned.by.anthy$tip.label)
-#which(pruned.by.anthy$tip.label%in%anthy$name)
-#pruned.by.anthy$node.label<-""
-#comparative.data(pruned.by.anthy,anthy,names.col=name,na.omit=FALSE)
+final.df$pro[final.df$Phen.sequence == "pro"] <- 1
+final.df$pro[final.df$Phen.sequence == "pro/syn"] <- 2
+final.df$pro[final.df$Phen.sequence== "syn"] <- 3
+final.df$pro[final.df$Phen.sequence== "syn/ser"] <- 4
+final.df$pro[final.df$Phen.sequence== "ser"] <- 5  
+final.df$pro[final.df$Phen.sequence== "hyst"] <- 6  
+####now for functional
+final.df["func.pro"]<-NA
+final.df$func.pro[final.df$Phen.sequence == "pro"] <- 1
+final.df$func.pro[final.df$Phen.sequence == "pro/syn"] <- 1
+final.df$func.pro[final.df$Phen.sequence== "syn"] <- 2
+final.df$func.pro[final.df$Phen.sequence== "syn/ser"] <- 2
+final.df$func.pro[final.df$Phen.sequence== "ser"] <- 3  
+final.df$func.pro[final.df$Phen.sequence== "hyst"] <- 3 
+##proteranthous as bianary
+final.df["bin.pro"]<-NA
+final.df$bin.pro[final.df$Phen.sequence == "pro"] <- 1
+final.df$bin.pro[final.df$Phen.sequence == "pro/syn"] <- 1
+final.df$bin.pro[final.df$Phen.sequence== "syn"] <- 0
+final.df$bin.pro[final.df$Phen.sequence== "syn/ser"] <- 0
+final.df$bin.pro[final.df$Phen.sequence== "ser"] <- 0  
+final.df$bin.pro[final.df$Phen.sequence== "hyst"] <- 0 
 
  ##plot on tree
+plot.phylo(pruned.by.anthy,show.tip.label = TRUE, tip.color= final.df$func.pro, adj=.4,align.tip.label = TRUE, cex = 0.5)
 plot.phylo(pruned.by.anthy,show.tip.label = TRUE, tip.color= final.df$pro, adj=.4,align.tip.label = TRUE, cex = 0.5)
+plot.phylo(pruned.by.anthy,show.tip.label = TRUE, tip.color= final.df$bin.pro, adj=.4,align.tip.label = TRUE, cex = 0.5)
 ###this worked! green=proteranthous, red= non-proteranthous, black= NA
-###method below doesnt work
-x<-final.df$Proteranthous
-names(x)=pruned.by.anthy$tip.label
 
-phylosig(pruned.by.anthy, x, method="lambda", test=TRUE, nsim=1000, se=NULL, start=NULL,
-         control=list())
-help("phylosig")
+##try plotting with picante
+traits<-dplyr::select(final.df,name,pro,bin.pro)
+#par(mfrow=c(2,2))
+ for (i in names(traits)) {
+   plot(anthy, show.tip.label=FALSE, main=i)
+   tiplabels(pch=22, col=traits[,i]+1, bg=traits[,i]+1, cex=1.5) }
+
+
+###preparing to calculate lamda
+#x<-final.df$pro #something seems wrong
+#names(x)=pruned.by.anthy$tip.label
+
+#y<-final.df$func.pro
+#names(y)=pruned.by.anthy$tip.label
+
+#z<-final.df$bin.pro
+#names(z)=pruned.by.anthy$tip.label
+####lamda test- these are all the same
+#phylosig(pruned.by.anthy, x, method="lambda", test=TRUE, nsim=1000, se=NULL, start=NULL,
+#         control=list())
+#phylosig(pruned.by.anthy, y, method="lambda", test=TRUE, nsim=1000, se=NULL, start=NULL,
+#         control=list())
+#phylosig(pruned.by.anthy, z, method="lambda", test=TRUE, nsim=1000, se=NULL, start=NULL,
+#         control=list())
+###nacho says not to use lamda because its for continuous traits
+which(final.df$name%in%pruned.by.anthy$tip.label)
+which(pruned.by.anthy$tip.label%in%final.df$name)
+pruned.by.anthy$node.label<-""
+
+d<-comparative.data(pruned.by.anthy,final.df,name, na.omit = FALSE)
+phylo.d(d$final.df,pruned.by.anthy,names.col = name ,binvar =bin.pro ,permut = 1000, rnd.bias=NULL)
 
