bdf8e0e6321cc47bd773f4d1979a34d4b828959b
dbuona <dbuonaiuto@g.harvard.edu>
2017-04-25 17:35:20 -0400

    worked to calculate k

--- Scripts/proteranty_tree_bianary.R
+++ Scripts/proteranty_tree_bianary.R
@@ -51,13 +51,13 @@ final.df$pro[final.df$Phen.sequence== "hyst"] <- 0
 
 ###now make pollination syndrom discrete
 final.df["pol"]<-NA
-final.df$pol[final.df$Pollination == "wind"] <- 4
-final.df$pol[final.df$Pollination == "ambo"] <- 3
-final.df$pol[final.df$Pollination == "insect"] <- 2
+final.df$pol[final.df$Pollination == "wind"] <- 9
+final.df$pol[final.df$Pollination == "ambo"] <- 8
+final.df$pol[final.df$Pollination == "insect"] <- 7
 
 ###make factors for bianary traits
-#final.df$pro<-as.factor(final.df$pro)
-#final.df$pol<-as.character(final.df$pol)
+final.df$pro<-as.integer(final.df$pro)
+final.df$pol<-as.integer(final.df$pol)
 
 ##subset down to relevant columns
 final.df<-select_(final.df, "name", "pro", "pol")
@@ -67,36 +67,22 @@ which(final.df$name%in%pruned.by.anthy$tip.label)
 which(pruned.by.anthy$tip.label%in%final.df$name)
 pruned.by.anthy$node.label<-""
 d<-comparative.data(pruned.by.anthy,final.df,name, na.omit = FALSE)
-
+d
 ##calculate phylo.d ### this syntax makes an error
 phylo.d(data = final.df,phy = pruned.by.anthy, names.col = name, binvar = pro, permut = 1000, rnd.bias = NULL)
 ##try another way, comparative data objects should be able to just run
-B <- comparative.data(pruned.by.anthy, final.df,name, na.omit=FALSE)
-phylo.d(B, binvar=pro)
 
-#Visualize I cant get the phytools to run:
-#http://blog.phytools.org/2014/08/plotting-multiple-binary-characters-at.html
-T<-sapply(pruned.by.anthy,function(x) as.numeric(getStates(x,"tips")))
-rownames(T)<-$tip.label
-class(pruned.by.anthy)
-getStates(pruned.by.anthy,"tips")
+phylo.d(d, binvar=pro)
 
 ###try with phylobase http://www2.hawaii.edu/~mbutler/PDFs/Ch10.Phylobase.pdf
 ###only works as continuous with column labels
 library(tidyverse)
 final.df<-  final.df %>% remove_rownames %>% column_to_rownames(var="name")
 head(final.df)
-final.df$pro<-as.numeric(final.df$pro)
-final.df$pol<-as.numeric(final.df$pol)
-tree4 <- as( pruned.by.anthy, "phylo4")
-attributes(tree4)
-tree4d <- phylo4d( tree4, tip.data=final.df)
-treePlot(tree4d)
-
 #####picante
 #https://cran.r-project.org/web/packages/picante/vignettes/picante-intro.pdf
-final.df$pro<-as.numeric(final.df$pro)
-final.df$pol<-as.numeric(final.df$pol)
+final.df$pro<-as.integer(final.df$pro)
+final.df$pol<-as.integer(final.df$pol)
 par(mfrow=c(1,2))
  for (i in names(final.df)) {
    plot(pruned.by.anthy, show.tip.label=FALSE, main=c(i))
@@ -104,17 +90,7 @@ par(mfrow=c(1,2))
  }
 ###signal for traits with picante
 final.df <- final.df[pruned.by.anthy$tip.label,]
-multiPhylosignal(final.df,pruned.by.anthy)
-###pretiest plots
-#http://blog.phytools.org/2016/02/dottree-for-discrete-character-data.html
-par(mfrow=c(1,1))
-final.df$pro<-as.factor(final.df$pro)
-final.df$pol<-as.character(final.df$pol)
-colors<-setNames(c("pink","yellow","green","light blue", "navy blue","orange" ),c("pro","syn","ser","wind","ambo","insect"))
+View(multiPhylosignal(final.df,pruned.by.anthy))
 
-dotTree(pruned.by.anthy,final.df,colors=colors,data.type="discrete",fsize=0.7)
 
-###plot for hysteranthy only
-pro.df<-select_(final.df, "pro")
-dotTree(pruned.by.anthy,pro.df,fsize=0.7,ftype="i")
 
