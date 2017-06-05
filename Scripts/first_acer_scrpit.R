88ee7f833c5ac53ddea47ac64d8db310489222de
dbuona <dbuonaiuto@g.harvard.edu>
2017-06-05 18:01:41 -0400

    Working on acer

--- Scripts/first_acer_scrpit.R
+++ Scripts/first_acer_scrpit.R
@@ -16,9 +16,11 @@ library(picante)
 library(phylobase)
 setwd("~/Documents/git/proterant/Input")
 ##read in tree, and data
-maple<-read.nexus("T5304.nex")
+mapl<-read.nexus("T5305.nex") ## no branch lengths
+maple<-read.nexus("T5304.nex") ##https://treebase.org/treebase-web/search/treeSearch.html oe "T5305.nex" from Renner et al 2008 no branch lengths
+read.nexus("T5297.nex") # unrooted no branch lengths
+read.nexus("T5299.nex") # unrooted no branch lengths
 names.intree<-maple$tip.label
-
 
 map.dat<-read.csv("acer_data.csv")
 map.dat$name<-paste("Acer",map.dat$specie_source,sep="_")
@@ -40,8 +42,21 @@ library(tidyverse)
 final.df<-  final.df %>% remove_rownames %>% column_to_rownames(var="name")
 head(final.df)
 pro.df<-select_(final.df, "anthy")
-colors2<-setNames(c("red","orange","yellow" ,"light blue","green","dark green"),c("hysteranthous","synanthous/hysteranthous","hysteranthous/synanthous","synanthous","synanthous/seranthous", "seranthous"))
+colors2<-setNames(c("red","red","red" ,"light blue","dark green","dark green"),c("hysteranthous","synanthous/hysteranthous","hysteranthous/synanthous","synanthous","synanthous/seranthous", "seranthous"))
 dotTree(pruned.by.anthy,pro.df,legend=TRUE,fsize=.8,ftype="i",colors=colors2)
+
+final.df$pro<-as.integer(final.df$pro)
+final.df$pol<-as.integer(final.df$pol)
+par(mfrow=c(1,2))
+for (i in names(final.df)) {
+  plot(pruned.by.anthy, show.tip.label=FALSE, main=c(i))
+  tiplabels(pch=22, col=final.df[,i]+1, bg=final.df[,i]+1, cex=0.5)
+}
+###signal
+library("phylolm")
+mod4a<-phyloglm(anthy~1,final.df, pruned.by.anthy, method = "logistic_MPLE", btol = 10, log.alpha.bound = 4,
+                start.beta=NULL, start.alpha=NULL,
+                boot = 0, full.matrix = TRUE)
 
 
 
