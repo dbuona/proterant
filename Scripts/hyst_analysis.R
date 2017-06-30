df13eeded6de5d5050ec91fc4ec0c4482395ec9c
dbuona <dbuonaiuto@g.harvard.edu>
2017-06-30 15:56:29 -0400

    Played with models
    different estimates for pals and arms, should play with priors:
    try this
    https://cran.r-project.org/web/packages/brms/vignettes/brms_nonlinear.ht
    ml#introduction for question about bernoulli

--- Scripts/hyst_analysis.R
+++ Scripts/hyst_analysis.R
@@ -31,67 +31,37 @@ final.df<-  final.df %>% remove_rownames %>% column_to_rownames(var="name")
 #what is the data structure?
 lapply(final.df, class) ### does this matter for bianary?
 
-######PGLS models for shrub/tree bianary and pollin syndrome #################
-mod3<-glm(pro~pol+class2,family = binomial(link="logit"),data=final.df)
-summary(mod3)
 
-
-###same as above by phylogenetically corrected
-mod3a<-phyloglm(pro~pol+class2,final.df, pruned.by.anthy, method = "logistic_MPLE", btol = 10, log.alpha.bound = 4,
-                start.beta=NULL, start.alpha=NULL,
-                boot = 0, full.matrix = TRUE)
-summary(mod3a)
-
-
-####phylogenetically correlated with synanthy coded as hysteranthy########
-mod3aa<-phyloglm(pro2~pol+class2,final.df, pruned.by.anthy, method = "logistic_MPLE", btol = 10, log.alpha.bound = 4,
-                start.beta=NULL, start.alpha=NULL,
-                boot = 0, full.matrix = TRUE)
-summary(mod3aa)
-
-####full model with everything bianary- exclude flo_type#########
-full.mod<-glm(pro~pol+class2+shade_bin+fruit_bin,family = binomial(link="logit"),data=final.df)
+####full model with everything bianary#########
+full.mod<-glm(pro~pol*class2*shade_bin*fruit_bin,family = binomial(link="logit"),data=final.df)
 summary(full.mod)
 
-####full binanry but phylogentically corrected###########
-full.modA<-phyloglm(pro~pol+class2+shade_bin+fruit_bin,final.df, pruned.by.anthy, method = "logistic_MPLE", btol = 10, log.alpha.bound = 4,
+####full  phylogentically corrected###########
+full.modA<-phyloglm(pro~pol+class2+shade_bin+fruit_bin+flo_type,final.df, pruned.by.anthy, method = "logistic_MPLE", btol = 10, log.alpha.bound = 4,
                 start.beta=NULL, start.alpha=NULL,
                 boot = 0, full.matrix = TRUE)
 summary(full.modA)
-coef(full.modA)
-vcov(full.modA)
-help(vcov)
-inv.logit(coef(full.modA)[1])
-inv.logit(coef(full.modA)[1]+coef(full.modA)[2])
-inv.logit(coef(full.modA)[1]+coef(full.modA)[2]+coef(full.modA)[5])
-inv.logit(coef(full.modA)[1]+coef(full.modA)[5])
 
 ###full phylogenetically, with hysteranthy to include synanthy
-full.modAA<-phyloglm(pro2~pol+class2+shade_bin+fruit_bin,final.df, pruned.by.anthy, method = "logistic_MPLE", btol = 10, log.alpha.bound = 4,
+full.modAA<-phyloglm(pro2~pol+class2+shade_bin+fruit_bin+flo_type,final.df, pruned.by.anthy, method = "logistic_MPLE", btol = 10, log.alpha.bound = 4,
                     start.beta=NULL, start.alpha=NULL,
                     boot = 0, full.matrix = TRUE)
 summary(full.modAA)
 
-coef(full.modAA)
-inv.logit(coef(full.modAA)[1])
-inv.logit(coef(full.modAA)[1]+coef(full.modAA)[2])
-inv.logit(coef(full.modAA)[1]+coef(full.modAA)[2]+coef(full.modA)[5])
-inv.logit(coef(full.modAA)[1]+coef(full.modAA)[5])
-vcov(full.modAA)
-
-##metrics
-#wind pollinated shrubs
-dim(filter(final.df, pol==1 & class=="0shrub"))
-dim(filter(final.df, pol==1 & class2==1))
-dim(filter(final.df,pol==1 &flo_type=="0perfect"& flo_type=="0perfect")) #just elms
-dim(filter(final.df,pol==1 &flo_type=="0perfect" & class=="0shrub")) ##doesnt exist 
-#stop("just stop for now")
 
 #########That was fun####################Nowdoit in BRMS############################################
 
 library("brms")
 library("MCMCglmm")
 #https://cran.r-project.org/web/packages/brms/README.html for some guideance
+
+#make all variable character for brms
+final.df$pro<-as.character(final.df$pro)
+final.df$pol<-as.character(final.df$pol)
+final.df$class2<-as.character(final.df$class2)
+final.df$shade_bin<-as.character(final.df$shade_bin)
+final.df$fruit_bin<-as.character(final.df$fruit_bin)
+
 
 ##construct covarience matrix:
 inv.phylo <- MCMCglmm::inverseA(pruned.by.anthy, nodes = "TIPS", scale = TRUE)
@@ -100,7 +70,7 @@ rownames(A) <- rownames(inv.phylo$Ainv)
 final.df<-rownames_to_column(final.df, "name")
 
 ###Best model so far
-model <- brm(pro~ pol+fruit_bin+shade_bin + (1|name), data = final.df, 
+model <- brm(pro~ pol+class2+fruit_bin+shade_bin +flo_type+ (1|name), data = final.df, 
  family = bernoulli(link="logit"), cov_ranef = list(pruned.by.anthy= A),iter=10000,
  prior = c(prior(normal(0, 5), "b"),
  prior(normal(0, 5), "Intercept"),
@@ -111,21 +81,22 @@ plot(marginal_effects(model, probs = c(0.05, 0.95)))
 
 ### not as good model
 model2 <- brm(pro~ pol+class2 +fruit_bin+shade_bin+ (1|name), data = final.df, 
-                    family = bernoulli(link="logit"), cov_ranef = list(pruned.by.anthy= A),iter=5000,
-                    prior = c(prior(cauchy(0, 2.5), "b"),
-                              prior(cauchy(0, 2.5), "Intercept"),
-                              prior(student_t(3,0, 5), "sd")))   
-######need help determining priors--and interpretting output. ##also why do i get divergent transitions with more inters This provides very different estimate than plgs
-pairs(model2)
+                    family = bernoulli(link="logit"), cov_ranef = list(pruned.by.anthy= A),iter=10000,
+                    prior = c(prior(cauchy(0, 5), "b"),
+                              prior(cauchy(0, 5), "Intercept"),
+                              prior(student_t(3,0, 10), "sd")))   
+
+
 summary(model2)
 
 library(shinystan)                   
 launch_shiny(model, rstudio = getOption("shinystan.rstudio"))
 
+
 #####compute phylo.d########################################################
 #final.df<-rownames_to_column(final.df  ,var="rowname")
 #phylo.d(data = final.df,phy = pruned.by.anthy, names.col = name, binvar = pro, permut = 1000, rnd.bias = NULL)
 
-
+myy<-final.df$pro
 
 
