989ac519bd8d855cbe819225a27b95fe569d40c5
dbuona <dbuonaiuto@g.harvard.edu>
2018-01-04 17:04:39 -0500

    Worked on manuscript

--- writting/concept.paper.analysis.R
+++ writting/concept.paper.analysis.R
@@ -29,7 +29,7 @@ mich.data$av_fruit_time<-as.numeric(mich.data$av_fruit_time)
 
 mich.data$fruiting<-NA
 mich.data$fruiting<-mich.data$av_fruit_time
-mich.data$fruiting[mich.data$fruiting==19]<-7
+#mich.data$fruiting[mich.data$fruiting==19]<-7
 mich.data$fruiting[mich.data$fruiting=="persistant"]<-12
 mich.data$fruiting[mich.data$fruiting=="persitant"]<-12
 mich.data$fruiting[mich.data$fruiting=="unreported"]<-9                                      
@@ -42,7 +42,6 @@ mich.data$pro3[mich.data$Phen.sequence== "syn"] <- 0
 mich.data$pro3[mich.data$Phen.sequence== "syn/ser"] <- 0
 mich.data$pro3[mich.data$Phen.sequence== "ser"] <- 0 
 mich.data$pro3[mich.data$Phen.sequence== "hyst"] <- 0
-
 
 ###phylo.D
 set.seed(122)
@@ -65,23 +64,25 @@ mich.data$av_fruit_time_cent<-(mich.data$av_fruit_time-mean(mich.data$av_fruit_t
 
 ###prepare for modeling
 mich.data<-  mich.data %>% remove_rownames %>% column_to_rownames(var="name")
-
+earl<-  earl%>% remove_rownames %>% column_to_rownames(var="name")
+mich.data$height10<-mich.data$heigh_height/10
 ###models:
-mich5<-phyloglm(pro~pol+heigh_height+flo_time+fruiting+shade_bin,mich.data, mich.tree, method = "logistic_MPLE", btol = 100, log.alpha.bound = 10,
+mich5<-phyloglm(pro2~pol+height10+flo_time+fruiting+shade_bin,mich.data, mich.tree, method = "logistic_MPLE", btol = 100, log.alpha.bound = 10,
                 start.beta=NULL, start.alpha=NULL,
                 boot=50,full.matrix = TRUE)
-
-mich5cent<-phyloglm(pro~pol+height_cent+flo_cent+fruit_cent+shade_bin,mich.data, mich.tree, method = "logistic_MPLE", btol = 100, log.alpha.bound = 10,
-                    start.beta=NULL, start.alpha=NULL,
-                    boot=50,full.matrix = TRUE)
+summary(mich5)
 
 Mich5cent.funct<-phyloglm(pro2~pol+height_cent+flo_cent+fruit_cent+shade_bin,mich.data, mich.tree, method = "logistic_MPLE", btol = 100, log.alpha.bound = 10,
                           start.beta=NULL, start.alpha=NULL,
                           boot=50,full.matrix = TRUE)
 
+summary(Mich5cent.funct)
+
 Mich5cent.super<-phyloglm(pro3~pol+height_cent+flo_cent+fruit_cent+shade_bin,mich.data, mich.tree, method = "logistic_MPLE", btol = 100, log.alpha.bound = 10,
                           start.beta=NULL, start.alpha=NULL,
                           boot=50,full.matrix = TRUE)
+summary(Mich5cent.super)
+
 ###effect plots
 bootest<-as.data.frame(Mich5cent.funct$coefficients)
 bootconf<-as.data.frame(Mich5cent.funct$bootconfint95)
@@ -101,7 +102,8 @@ bootmich$trait[bootmich$trait=="fruit_cent"]<-"fruit timing"
 bootmich$trait[bootmich$trait=="flo_cent"]<-"flower timing"
 
 functplot<-ggplot(bootmich,aes(estimate,trait))+geom_point()+geom_segment(aes(y=trait,yend=trait,x=low,xend=high))+ggtitle("Functional Hysteranthy")+theme_light()+geom_vline(aes(xintercept=0,color="red"))+theme(plot.title = element_text(hjust = 0.5))+guides(color="none")
-####now phys
+
+####now physiological
 bootest<-as.data.frame(Mich5cent.super$coefficients)
 bootconf<-as.data.frame(Mich5cent.super$bootconfint95)
 bootconf<-as.data.frame(t(bootconf))
@@ -121,3 +123,65 @@ bootmich$trait[bootmich$trait=="flo_cent"]<-"flower timing"
 
 physplot<-ggplot(bootmich,aes(estimate,trait))+geom_point()+geom_segment(aes(y=trait,yend=trait,x=low,xend=high))+ggtitle("Physiological Hysteranthy")+theme_light()+geom_vline(aes(xintercept=0,color="red"))+theme(plot.title = element_text(hjust = 0.5))+guides(color="none")
 
+
+
+
+###average predictive comparsons using uncentered data except for height because it wouldnt converge
+beta<-coef(mich5)
+hi<-1
+lo<-0
+###for [pollination syndrome]
+beta[2]
+delta<-invlogit(beta[1]+beta[2]*hi+beta[3]*mich.data$height10+beta[4]*mich.data$flo_time+beta[5]*mich.data$fruiting+beta[6]*mich.data$shade_bin)-
+  invlogit(beta[1]+beta[2]*lo+beta[3]*mich.data$height10+beta[4]*mich.data$flo_time+beta[5]*mich.data$fruiting+beta[6]*mich.data$shade_bin)
+
+print(mean(delta))
+
+###for flowering
+earl<-4
+mid<-5
+delta<-invlogit(beta[1]+beta[2]*mich.data$pol+beta[3]*mich.data$height10+beta[4]*earl+beta[5]*mich.data$fruiting+beta[6]*mich.data$shade_bin)-
+  invlogit(beta[1]+beta[2]*mich.data$pol+beta[3]*mich.data$height10+beta[4]*mid+beta[5]*mich.data$fruiting+beta[6]*mich.data$shade_bin)
+
+print(mean(delta))
+
+
+
+
+###inter anual variation example not looking great
+
+ts<-read.csv("individual_phenometrics_data.csv",header=TRUE)
+ts<-dplyr::select(ts,Individual_ID,Phenophase_ID,Common_Name,Phenophase_Description,First_Yes_Year,First_Yes_DOY)
+ts$First_Yes_Year<-as.character(ts$First_Yes_Year)
+ts<-filter(ts, First_Yes_Year!="2015")
+unique(ts$Phenophase_Description)
+ts.func<-filter(ts, Phenophase_Description %in% c("Flowers or flower buds","Increasing leaf size"))
+table(ts.func$Phenophase_Description)
+q<-ggplot(ts.func, aes(x=First_Yes_Year, y=First_Yes_DOY, color=Phenophase_Description)) +
+  stat_summary()+labs(title="tree spotters", x="Year", y="Days since initiation")
+q+facet_wrap(~Common_Name)
+
+#Harvard forest
+hf<-read.csv("hf003-05-mean-ind.csv",header=TRUE)
+hf<-filter(hf,year>2004)
+hf$year<-as.integer(hf$year)
+###plot the budburst data###
+new<-rename(hf,lbb.jd=bb.jd)
+head(new)
+new<-dplyr::select(new,-l75.jd)
+head(new)
+new<-gather(new,phenophase,eventday,lbb.jd:fbb.jd)
+###Filter out species with insufficient flowering records
+new<-filter(new, species %in% c( "ACRU","BEAL","QURU"))
+q<-ggplot(new, aes(x=year, y=eventday, color=phenophase)) +
+  stat_summary()+labs(title="Flower and Leaf Budburst at Harvard Forest", x="Year", y="Days since initiation")
+q+facet_wrap(~species)
+##Now plot flowers open vs. l75
+new2<-dplyr::select(hf,-fbb.jd)
+head(new2)
+new2<-gather(new2,phenophase,eventday,l75.jd:fopn.jd)
+###Filter out species with insufficient flowering records
+new2<-filter(new2, species %in% c("ACRU","BEAL","QURU"))
+q<-ggplot(new2, aes(x=year, y=eventday, color=phenophase)) +
+  stat_summary()+labs(title="Flowering and Leaf Expansion at Harvard Forest", x="Year", y="Days since initiation")
+q+facet_wrap(~species)
