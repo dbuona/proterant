d7cc64ec1a7ae5b9df5632f5b0485c9757c8c96b
dbuona <dbuonaiuto@g.harvard.edu>
2019-12-06 13:57:45 -0500

    worked on plots

--- sub_projs/HarvardForest/makeHFplots.R
+++ sub_projs/HarvardForest/makeHFplots.R
@@ -16,10 +16,10 @@ HF<-read.csv("HarvardForest/hf003-05-mean-ind.csv",header=TRUE)
 
 ###give them species names
 HF$name[HF$species=="ACPE"]<-"A. pensylvanicum"
-HF$name[HF$species=="ACRU"]<-"*A. rubrum"
+HF$name[HF$species=="ACRU"]<-"A. rubrum"
 HF$name[HF$species=="ACSA"]<-"*A. saccharrum"
 HF$name[HF$species=="AMSP"]<-"Amelanchier spp."
-HF$name[HF$species=="BEAL"]<-"*B. allegheniensis"
+HF$name[HF$species=="BEAL"]<-"B. allegheniensis"
 HF$name[HF$species=="BELE"]<-"*B. lenta"
 HF$name[HF$species=="BEPA"]<-"*B. papyrifera"
 HF$name[HF$species=="BEPO"]<-"*B. populifolia"
@@ -47,10 +47,37 @@ colnames(HF)
 colnames(HF)<-c("year" , "tree.id", "species","leaf budburst","leaf expansion (75%)","flower budburst","flower open","name","physiological offset","functional offset","intermediate offset")
 HF2<-tidyr::gather(HF, phase,DOY,4:7)
 HF2<-filter(HF2,!is.na(name))
-jpeg("HarvardForest/HFmeans_expanded.jpeg",width = 8, height = 6, units = 'in', res=300)
-ggplot(HF2,(aes(name,DOY)))+stat_summary(aes(shape=phase,color=phase))+scale_color_manual(values=c("darkgray","darkgray","black","black"))+scale_shape_manual(values=c(2,1,17,16))+theme_linedraw()+ylab("Day of Year")+xlab(NULL)+theme(axis.text.x = element_text(angle = 300,hjust=0,face="italic"))
+HFshort<-dplyr::filter(HF2,name %in% c("A. rubrum","Amelanchier spp."))
+
+#jpeg("HarvardForest/HFmeans_expanded.jpeg",width = 8, height = 6, units = 'in', res=300)
+a<-ggplot(HFshort,(aes(name,DOY)))+stat_summary(aes(shape=phase,color=phase))+scale_color_manual(values=c("darkgray","darkgray","black","black"))+scale_shape_manual(values=c(2,1,17,16))+theme_linedraw()+ylab("Day of Year")+xlab(NULL)+theme(axis.text.x = element_text(hjust=0,face="italic"))
 dev.off()
 
+####does the graph work for these species
+shorty<-filter(HF,name %in% c("Amelanchier spp.","A. rubrum"))
+shorty<-filter(shorty,year<2002)
+colnames(shorty)
+shorty$FLS<-ifelse(shorty$"physiological offset"<0,"seranthous","hysteranthous")
+#colnames(shorty)<-c("year","tree.id","species","leaf_budburst","leaf_expansion(75%)","flower_budburst","flower_open","physiological_offset","functional_offset","name","FLS")
+shorty<-drop_na(shorty)
+#shorty$tree.id[shorty$tree.id=="shorty-01"]<-"1"
+#shorty$tree.id[shorty$tree.id=="shorty-03"]<-"3"
+#shorty$tree.id[shorty$tree.id=="shorty-04"]<-"4"
+
+colnames(shorty)[6]<-"flower.budburst"
+colnames(shorty)[4]<-"leaf.budburst"
+#jpeg("HarvardForest/HF_Q_ru_interannual.jpeg",width = 8, height = 6, units = 'in', res=350)
+pd2<-position_dodge2(0.4)
+
+b<-ggplot(shorty,aes(year,flower.budburst))+geom_point(aes(year,flower.budburst,color=tree.id,group = row.names(shorty),shape="flower.budburst") ,position=pd2)+
+  geom_point(aes(year,leaf.budburst,color=tree.id,group = row.names(shorty),shape="leaf.budburst"),position=pd2)+
+  geom_linerange(aes(x=year,ymin=flower.budburst,ymax=leaf.budburst, linetype=FLS,color=tree.id,group = row.names(shorty)),position=pd2)+
+  theme_linedraw()+labs(y = "Day of year",color= "Tree I.D.")+
+  scale_color_brewer(palette="Set1")+
+  scale_shape_manual(name="Phenophase",values=c(2,17), label=c("flower budburst","leaf budburst"))+facet_wrap(~name,nrow=2)
+
+dev.off()
+ggpubr::ggarrange(a,b)
 
 ########Analysis 3 Quercus rubra at HF.........
 QURU<-filter(HF,species=="QURU")
