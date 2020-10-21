865218f6209151ca90017d326955b2aee4738dba
dbuona <dbuonaiuto@g.harvard.edu>
2020-10-21 09:52:30 -0400

    new draft of flo_buddy

--- investment/prunocerasus.R
+++ investment/prunocerasus.R
@@ -10,28 +10,41 @@ library(ggplot2)
 library("brms")
 
 sp<-read.csv("herbaria_prunus_rec.csv")
-(unique(sp$specificEpithet))
-pruno<-c("alleghaniensis","angustifolia","americana" ,"gracilis","geniculata","hortulana" ,"maritima",
-  "mexicana","murrayana","munsoniana","nigra","rivularis","umbellata","subcordata","texana" )
 
-  
-library(dplyr)     
-
-prunocerasus<-dplyr::filter(sp, specificEpithet %in% pruno)
+allpru<-sp %>%group_by(specificEpithet)%>% count() %>% arrange(desc(n))
 
 geoCounty$rMapState<-str_to_title(geoCounty$rMapState) ### centriod coordinates for all US counties
 colnames(geoCounty)[6]<-"stateProvince" ## make column names compatible
+prunus.data<-dplyr::left_join(sp,geoCounty,by=c("county","stateProvince"))
 
-prunocerasus<-dplyr::left_join(prunocerasus,geoCounty,by=c("county","stateProvince")) ## This assigns each country coordinates
+geo.sp<-filter(prunus.data,county!="")
+allpru.geo<-geo.sp %>%group_by(specificEpithet)%>% count() %>% arrange(desc(n))
+all.specs<-merge(allpru,allpru.geo,by="specificEpithet")
+colnames(all.specs)[c(2,3)]<-c("n","n.wcounty")
+
+all.specs<-all.specs %>% arrange(desc(n.wcounty))
+ ## This assigns each country coordinates
+
+
+
+pruno<-c("alleghaniensis","angustifolia","americana" ,"gracilis","geniculata","hortulana" ,"maritima",
+  "mexicana","murrayana","munsoniana","nigra","rivularis","umbellata","subcordata","texana" )
+
+
+xtable::xtable(all.specs)
+
+prunocerasus<-dplyr::filter(all.specs, specificEpithet %in% pruno)
+
 
 
 palmer.b <- brick("lbda-v2_kddm_pmdi_2017.nc")  ## read in balmer drought index data 
-plot(palmer.b)
+
+plot(palmer.b[[1900:2000]])
 lonpoints<-prunocerasus$lon # make vector of prunus coordinates
 latpoints<-prunocerasus$lat #
 extract.pts <- cbind(lonpoints,latpoints) #make coordinates to extract
 ext <- extract(palmer.b,extract.pts,method="simple") ###extract drought info from each coordinate of prunus colllect
-head(ext)
+
 
 
 mean.prunus <- calc(palmer.b, fun = mean,na.rm=TRUE) #average palmer drought index acrosss time
@@ -60,7 +73,7 @@ pruno<-c("alleghaniensis","angustifolia","americana" ,"gracilis","geniculata","h
          "mexicana","murrayana","munsoniana","nigra","rivularis","umbellata","subcordata","texana" )
 
 prunycer<-dplyr::filter(hyster,specificEpithet %in% pruno)
-
+prunycer2<-left_join(prunycer,prunocerasus)
 #d<-dplyr::filter(prunus.data.2,!is.na(FLS)) ### 7668 rows have county coordiates
 
 ggplot(prunycer,aes(FLS,mean_size))+geom_boxplot()+stat_summary(color="red")+
@@ -71,4 +84,8 @@ ggplot(prunycer,aes(hyst,mean_size))+geom_boxplot()+stat_summary(color="red")+
 
 ggplot(prunycer,aes(hyst2,mean_size))+geom_boxplot()+stat_summary(color="red")+
   ggthemes::theme_base()+xlab("FLS")+ylab("mean flower size (mm)")
+
+ggplot(prunycer2,aes(FLS,pdsi))+geom_boxplot()+stat_summary(color="red")+
+  ggthemes::theme_base()+xlab("FLS")+ylab("mean flower size (mm)")
+
 
