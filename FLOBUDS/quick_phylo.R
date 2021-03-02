ad9ca7f90dd2c871e4c9fccdad1663797caa53b2
dbuona <dbuonaiuto@g.harvard.edu>
2021-03-02 15:47:06 -0500

    quick code change

--- FLOBUDS/quick_phylo.R
+++ FLOBUDS/quick_phylo.R
@@ -23,7 +23,7 @@ names.intree<-treee$tip.label # names the names
 
 
 namelist<-c("Prunus_pensylvanica","Prunus_virginiana","Acer_rubrum","Acer_pensylvanicum",
-          "Ilex_mucronata","Ilex_verticillata","Vaccinium_corymbosum","Corylus_cornuta",
+          "Viburnum_acerifolium","Ilex_mucronata","Ilex_verticillata","Vaccinium_corymbosum","Corylus_cornuta",
           "Comptonia_peregrina") # these are species I want
 
 to.prune<-which(!names.intree%in%namelist) #prun the tree
@@ -38,7 +38,7 @@ intersect(namelist,mytree.names) #yes
 
 dat<-read.csv("flobudsdata.use.csv",header = TRUE) ## reaadin in pheno data
 dat<-dplyr::filter(dat,Chill==1) # subset to like conditions High chill
-dat<-dplyr::filter(dat,Force=="W") # high force
+#dat<-dplyr::filter(dat,Force=="W") # high force
 
 
 
@@ -46,10 +46,10 @@ dat$FLS<- dat$flo_day-dat$budburst.9. # calculate FLSs
 
 d<- dat %>% dplyr::group_by(GEN.SPA) %>% dplyr::summarise(meanFLS=mean(FLS,na.rm=TRUE)) ## take species mean
 
-d<-dplyr::filter(d,!GEN.SPA%in% c("ACE.SAC","BET.ALL","VIB.ACE","AME.SPP")) # remvoe species with no FLS data
+d<-dplyr::filter(d,!GEN.SPA%in% c("ACE.SAC","BET.ALL","AME.SPP","BET.SPP")) # remvoe species with no FLS data
 
 d$taxa<-c("Acer_pensylvanicum","Acer_rubrum","Comptonia_peregrina","Corylus_cornuta","Ilex_mucronata","Ilex_verticillata",
-          "Prunus_pensylvanica","Prunus_virginiana","Vaccinium_corymbosum") # add a cloumn matching the tree name formate
+          "Prunus_pensylvanica","Prunus_virginiana","Vaccinium_corymbosum","Viburnum_acerifolium") # add a cloumn matching the tree name formate
 
 ### put it in same order as tree
 final.df<-d[match(mytree.names, d$taxa),]
@@ -57,11 +57,15 @@ namelist2<-final.df$taxa
 namelist2==mytree.names # check 
 final.df$taxa== mytree.names #check
 setdiff(mytree.names,namelist2)
+is.ultrametric(pruned.by.anthy)
+
+pruned.by.anthy<-chronoMPL(pruned.by.anthy)
 
 ### try pic but im not sure how thisd work
 fls.pic<-pic(final.df$meanFLS,pruned.by.anthy,,var.contrasts = TRUE,scaled = TRUE)
 lm(fls.pic~1)
 
-instread just calculate a phyosignal
+#instread just calculate a phyosignal
 rownames(final.df)<-final.df$taxa
-phylosig(pruned.by.anthy,final.df$meanFLS,method="lambda",nsim = 100) ### 0.17
+phylosig(pruned.by.anthy,final.df$meanFLS,method="lambda",nsim = 100) ### 0.1r
+
