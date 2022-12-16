c048097f5093a9e0e216a81c1f43383b2857cd00
dbuona <dbuonaiuto@g.harvard.edu>
2022-12-16 15:17:45 -0500

    Revision tweaks

--- FLOBUDS/periodicity/concept_4_supp.R
+++ FLOBUDS/periodicity/concept_4_supp.R
@@ -6,18 +6,18 @@ z <- (y*-.2)+(x*-.04)+50
 forcing<-c(0,1,0,1,0,1)
 dat<-data.frame(x,y,z,photoperiod,forcing)
 
-ploa<-ggplot(dat,aes(x,z))+stat_smooth(method="lm",fullrange = FALSE,aes(color=photoperiod,linetype="estimated effect"),size=1.5)+stat_smooth(method="lm",fullrange = TRUE,size=0.5,aes(color=photoperiod,linetype="true effect"))+scale_color_viridis_d(option="turbo")+ggthemes::theme_few()+ylab("Day of budburst")+
+ploa<-ggplot(dat,aes(x,z))+stat_smooth(method="lm",fullrange = FALSE,aes(color=photoperiod,linetype="estimated effect"),size=1.5)+stat_smooth(method="lm",fullrange = TRUE,size=0.5,aes(color=photoperiod,linetype="true effect"))+scale_color_viridis_d(option="turbo")+ggthemes::theme_few(base_size = 9)+ylab("Day of budburst")+
   xlab("Thermal sums")+#+ylim(34,48)+
   scale_linetype_manual(name = "effect",
                         values = c( "estimated effect" = "solid", "true effect" = "dotted"))
 
-plob<-ggplot(dat,aes(forcing,z))+stat_smooth(method="lm",fullrange = FALSE,aes(color=photoperiod),size=1.5)+scale_color_viridis_d(option="turbo")+ggthemes::theme_few()+ylab("Day of budburst")+
+plob<-ggplot(dat,aes(forcing,z))+stat_smooth(method="lm",fullrange = FALSE,aes(color=photoperiod),size=1.5)+scale_color_viridis_d(option="turbo")+ggthemes::theme_few(base_size = 9)+ylab("Day of budburst")+
   scale_x_continuous(limits = c(-.25,1.25),breaks=c(0,.5,1),labels=c(labels=c(expression(""*20/10~degree*C)),expression(""*22.5/12.5~degree*C),expression(""*25/15~degree*C)))+xlab("Forcing treatment")#+
 
 ggpubr::ggarrange(ploa,plob,common.legend=TRUE,labels=c("a)","b)"))
 
-jpeg("~/Documents/git/proterant/FLOBUDS/Plots/periodicity_figures/apparent4spp.jpeg",width = 6,height=5,unit="in",res=200)
-ggpubr::ggarrange(ploa,plob,common.legend=TRUE,labels=c("a)","b)"))
+jpeg("~/Documents/git/proterant/FLOBUDS/Plots/periodicity_figures/apparent4spp.jpeg",width = 7,height=4,unit="in",res=200)
+ggpubr::ggarrange(ploa,plob,common.legend=TRUE,labels=c("a)","b)"),label.x = -.02)
 dev.off()
 
 
